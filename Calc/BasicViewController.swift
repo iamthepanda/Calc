2d6953b7b7d9775d303476111d0ec8546516fc7c
George Jone <g.george.jone@gmail.com>
2017-04-26 19:27:00 -0700

    created IBActions for basic buttons

---
+++ Calc/BasicViewController.swift
@@ -0,0 +1,66 @@
+//
+//  BasicViewController.swift
+//  Calc
+//
+//  Created by George Jone on 4/26/17.
+//  Copyright © 2017 George Jone. All rights reserved.
+//
+
+import UIKit
+
+class BasicViewController: UIViewController {
+    @IBAction func equal(_ sender: UIButton) {
+    }
+    @IBAction func backspace(_ sender: UIButton) {
+    }
+    @IBAction func enter(_ sender: UIButton) {
+    }
+    @IBAction func modulo(_ sender: UIButton) {
+    }
+    @IBAction func factorial(_ sender: UIButton) {
+    }
+    @IBAction func hat(_ sender: UIButton) {
+    }
+    @IBAction func sqrt(_ sender: UIButton) {
+    }
+    @IBAction func openParen(_ sender: UIButton) {
+    }
+    @IBAction func closeParen(_ sender: UIButton) {
+    }
+    @IBAction func dot(_ sender: UIButton) {
+    }
+    @IBAction func multiply(_ sender: UIButton) {
+    }
+    @IBAction func divide(_ sender: UIButton) {
+    }
+    @IBAction func plus(_ sender: UIButton) {
+    }
+    @IBAction func minus(_ sender: UIButton) {
+    }
+    @IBAction func x(_ sender: UIButton) {
+    }
+    @IBAction func euler(_ sender: UIButton) {
+    }
+    @IBAction func piButton(_ sender: UIButton) {
+    }
+    @IBAction func num0(_ sender: UIButton) {
+    }
+    @IBAction func num1(_ sender: UIButton) {
+    }
+    @IBAction func num2(_ sender: UIButton) {
+    }
+    @IBAction func num3(_ sender: UIButton) {
+    }
+    @IBAction func num4(_ sender: UIButton) {
+    }
+    @IBAction func num5(_ sender: UIButton) {
+    }
+    @IBAction func num6(_ sender: UIButton) {
+    }
+    @IBAction func num7(_ sender: UIButton) {
+    }
+    @IBAction func num8(_ sender: UIButton) {
+    }
+    @IBAction func num9(_ sender: UIButton) {
+    }
+}
---
+++ Calc/ExtraViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ExtraViewController.swift
+//  Calc
+//
+//  Created by George Jone on 4/26/17.
+//  Copyright © 2017 George Jone. All rights reserved.
+//
+
+
+import UIKit
+
+class ExtraViewController: UIViewController {
+    
+}
